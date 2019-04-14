e12fee62b07ea2aba4dc6f48e793696168484635
宫蒙恩 <2363878987@qq.com>
2019-04-14 18:12:04 +0800

    //this is IDEA test add a new bean "T1"

--- git_test/src/com/alibaba/test/T1.java
+++ git_test/src/com/alibaba/test/T1.java
@@ -3,5 +3,6 @@ package com.alibaba.test;
 public class T1 {
     public static void main(String[] args) {
         System.out.println("this is test id 001");
+        System.out.println("this is test id 002");
     }
 }
